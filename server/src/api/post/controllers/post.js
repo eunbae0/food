c5d526ba821e883aa8863a769406c9a594e32b09
은배 <geb9211@gmail.com>
2023-03-14 23:59:28 +0900

    feat: post api 수정

--- server/src/api/post/controllers/post.js
+++ server/src/api/post/controllers/post.js
@@ -1,9 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  * post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post");
+
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  async find(ctx) {
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+    const query = strapi.db.query("api::post.post");
+    await Promise.all(
+      data.map(async (item, index) => {
+        const foundItem = await query.findOne({
+          where: {
+            id: item.id,
+          },
+          populate: ["createdBy", "updatedBy"],
+        });
+
+        data[index].attributes.createdBy = {
+          id: foundItem.createdBy.id,
+          firstname: foundItem.createdBy.firstname,
+          lastname: foundItem.createdBy.lastname,
+        };
+        data[index].attributes.updatedBy = {
+          id: foundItem.updatedBy.id,
+          firstname: foundItem.updatedBy.firstname,
+          lastname: foundItem.updatedBy.lastname,
+        };
+      })
+    );
+    return { data, meta };
+  },
+}));
