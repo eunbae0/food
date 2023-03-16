34abad9abef9624db6e852f89ea9fb803c79a1b9
은배 <geb9211@gmail.com>
2023-03-16 15:38:20 +0900

    refactor: container 폴더 추가 및 수정

---
+++ web/src/containers/Main/index.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/header/Header';
+import Card from '@/components/card/Card';
+import styles from '@/styles/Main.module.css';
+
+export default function MainContainer() {
+  return (
+    <main>
+      <Header user={true} />
+      <section className={styles.bannerWrapper}></section>
+      <section className={styles.cardWrapper}>
+        <Card card={'asdf'} />
+        <Card card={'asdf'} />
+        <Card card={'asdf'} />
+      </section>
+    </main>
+  );
+}
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,7 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
-import Header from '@/components/header/Header';
-import Card from '@/components/card/Card';
-import styles from '../styles/Home.module.css';
+import MainContainer from '@/containers/Main';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export default function Home() {
+export default function Main() {
   return (
     <>
       <Head>
@@ -15,15 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <main>
-        <Header user={true} />
-        <section className={styles.bannerWrapper}></section>
-        <section className={styles.cardWrapper}>
-          <Card card={'asdf'} />
-          <Card card={'asdf'} />
-          <Card card={'asdf'} />
-        </section>
-      </main>
+      <MainContainer />
     </>
   );
 }
