5a98561423acd90e9678500a04a10779b3390feb
은배 <geb9211@gmail.com>
2023-03-24 18:01:14 +0900

    refactor: Head 정보 _app.tsx로 이동

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,16 +1,8 @@
-import Head from 'next/head';
 import MainContainer from '@/containers/Main';
+import { withUser } from '@/hoc/withUser';
 
-export default function Main() {
-  return (
-    <>
-      <Head>
-        <title>Food</title>
-        <meta name="description" content="food share" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-      <MainContainer />
-    </>
-  );
+function Main() {
+  return <MainContainer />;
 }
+
+export default withUser(Main);
