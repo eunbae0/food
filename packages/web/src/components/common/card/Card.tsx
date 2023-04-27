d5d76272e1b44429c8e64921885a21151609a272
은배 <geb9211@gmail.com>
2023-04-27 10:49:25 +0900

    fix: next image 적용 안되던 문제 해결

--- packages/web/src/components/common/card/Card.tsx
+++ packages/web/src/components/common/card/Card.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+
 import CardProps from './card.type';
 import styles from './Card.module.css';
 import { authAPI } from '@/api';
@@ -6,12 +8,17 @@ import { authAPI } from '@/api';
 import Button from '@/components/common/button/Button';
 
 export const Header = ({ data }: CardProps) => {
+  const router = useRouter();
+  const onClickCard = () => {
+    router.push(`/post/${data.id}`);
+  };
   return (
     <article className={styles.wrapper}>
       <header className={styles.header}>
         <div className={styles.headerLeft}>
           <Button primary={false}>
             <Image
+              loader={({ src }) => `${process.env.NEXT_PUBLIC_API_URL}${src}`}
               src={data.attributes.profileImg.url}
               width={40}
               height={40}
@@ -30,10 +37,7 @@ export const Header = ({ data }: CardProps) => {
           </Button>
         </div>
       </header>
-      <section
-        className={styles.section}
-        onClick={/*해당 글로 이동 로직*/ () => {}}
-      >
+      <section className={styles.section} onClick={onClickCard}>
         <Image src="" width={360} height={188} alt="" />
         <div className={styles.bottomWrapper}>
           <h2 className={styles.h2}>{data.attributes.title}</h2>
