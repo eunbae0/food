a6b50be42011dcabfe0606becd8344e54af95b26
은배 <geb9211@gmail.com>
2023-04-27 10:48:19 +0900

    feat: 메인페이지 Card 컴포넌트 렌더링

--- packages/web/src/containers/Main/index.tsx
+++ packages/web/src/containers/Main/index.tsx
@@ -6,21 +6,21 @@ import Banner from '@/components/common/banner/Banner';
 import Card from '@/components/common/card/Card';
 
 import styles from '@/styles/Main.module.css';
+import { PostDataArray } from '@/api/types/post.types';
 
-export default function MainContainer() {
-  const { isLogin, isLoading, user } = useSelector((state: indexState) => ({
-    isLogin: state.userData.isLogin,
-    isLoading: state.userData.isLoading,
-    user: state.userData.user,
-  }));
+export default function MainContainer({ data }: { data: PostDataArray }) {
+  // const { isLogin, isLoading, user } = useSelector((state: indexState) => ({
+  //   isLogin: state.userData.isLogin,
+  //   isLoading: state.userData.isLoading,
+  //   user: state.userData.user,
+  // }));
+
   return (
     <main>
       <Header />
       <Banner />
       <section className={styles.cardWrapper}>
-        <Card card={'asdf'} />
-        <Card card={'asdf'} />
-        <Card card={'asdf'} />
+        {!!data && data.map((data) => <Card key={data.id} data={data} />)}
       </section>
     </main>
   );
